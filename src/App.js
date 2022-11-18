5f960cb299b5c60a56675c8e15b5235d61d9ca14
bertwinkeller <bertwinkeller@gmail.com>
2022-11-17 23:05:53 -0800

    upload css files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import {useEffect} from 'react'
 
+//e68d7eeb
+
+const API_URL = 'http://www.omdbapi.com?apikey=e68d7eeb'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&S=${title}`)
+        const data = await response.json()
+
+        console.log(data.Search)
+    }
+
+
+    useEffect(() => {
+
+    searchMovies('Spiderman')
+        
+    }, [])
+
+
+
+
   return (
     <div>
         <h1>Hello</h1>
