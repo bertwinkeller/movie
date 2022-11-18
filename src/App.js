28b2be8fc3b7ac3d6a26d2413c56c7e0665d097b
bertwinkeller <bertwinkeller@gmail.com>
2022-11-17 23:34:19 -0800

    updated css

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import {useEffect} from 'react'
+import { useEffect } from "react"
 
 //e68d7eeb
+import "./App.css"
+import SearchIcon from "./search.svg"
+const API_URL = "http://www.omdbapi.com?apikey=e68d7eeb"
 
-const API_URL = 'http://www.omdbapi.com?apikey=e68d7eeb'
+const movie1 = {
+  Title: "Superman, Spiderman or Batman",
+  Year: "2011",
+  imdbID: "tt2084949",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg",
+}
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&S=${title}`)
+    const data = await response.json()
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&S=${title}`)
-        const data = await response.json()
+    console.log(data.Search)
+  }
 
-        console.log(data.Search)
-    }
-
-
-    useEffect(() => {
-
-    searchMovies('Spiderman')
-        
-    }, [])
-
-
-
+  useEffect(() => {
+    searchMovies("Spiderman")
+  }, [])
 
   return (
-    <div>
-        <h1>Hello</h1>
-      
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder=" Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
+          </div>
+
+          <div>
+            <span>
+                {movie1.Type}
+            </span>
+            <h3>
+                {movie1.Title}
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
