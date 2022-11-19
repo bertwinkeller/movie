e6016a7692c52555b9d2bd1ecc80f6521180a7fd
bertwinkeller <bertwinkeller@gmail.com>
2022-11-18 16:42:46 -0800

    remove placeholders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import MovieCard from "./MovieCard"
 
 //e68d7eeb
 import "./App.css"
@@ -15,6 +16,8 @@ const movie1 = {
 }
 
 const App = () => {
+  const [movies, setMovies] = useState([])
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&S=${title}`)
     const data = await response.json()
@@ -39,25 +42,20 @@ const App = () => {
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
 
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-
-          <div>
-            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
-          </div>
-
-          <div>
-            <span>
-                {movie1.Type}
-            </span>
-            <h3>
-                {movie1.Title}
-            </h3>
-          </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie}/>
+          ))}
         </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
+
+      <div className="container">
+        <MovieCard movie1={movie1} />
       </div>
     </div>
   )
